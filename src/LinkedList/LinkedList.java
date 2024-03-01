dd4edbe2e214cfc3caca72e196d306d3746b3c77
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-03-01 19:47:30 +0530

    LinkedList

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    private class Node {
+        private int value;
+        private Node next;
+    }
+}
