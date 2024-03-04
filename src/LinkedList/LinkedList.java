e7dd62eed79915b810e33b68668f18a4ec11e0b6
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-03-04 18:29:12 +0530

    Deletion in LinkedList

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -5,9 +5,12 @@ public class LinkedList {
     private Node tail;
     private int size;
 
-
     public LinkedList() {
         this.size = 0;
+    }
+
+    public void displaySize() {
+        System.out.println(this.size);
     }
 
     public void insertLast(int val) {
@@ -16,8 +19,29 @@ public class LinkedList {
             return;
         }
         Node node = new Node(val);
-        node.next = tail;
+        tail.next = node;
         tail = node;
+
+        size++;
+    }
+
+    public void insert(int val, int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = head.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
 
         size++;
     }
@@ -32,6 +56,67 @@ public class LinkedList {
         }
 
         size++;
+    }
+
+    public int deleteFirst() {
+        int value = head.value;
+        head = head.next;
+
+        if (head == null) {
+            tail = null;
+        }
+
+        size--;
+        return value;
+    }
+
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+
+        prev.next = prev.next.next;
+
+        return val;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+
+        tail = secondLast;
+        tail.next = null;
+
+        return val;
+    }
+
+    public Node find(int value) {
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
     }
 
     public void display() {
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -5,11 +5,24 @@ public class Main {
         System.out.println();
 
         LinkedList list = new LinkedList();
+
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(13);
 
+        list.insertLast(99);
+        // list.insert(100, 3);
+
         list.display();
+        System.out.println(list.find(2));
+        System.out.println(list.delete(2));
+        list.display();
+
+//        list.display();
+//        list.deleteFirst();
+//        System.out.println(list.deleteLast());
+//        list.displaySize();
+//        list.display();
     }
 }
