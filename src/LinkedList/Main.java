1106aafaacf93c6d5a32af77c653ab1aedf7006b
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-03-05 15:51:53 +0530

    DoublyLinkedList Implementation for Internal Working

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -6,23 +6,29 @@ public class Main {
 
         LinkedList list = new LinkedList();
 
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(13);
-
-        list.insertLast(99);
-        // list.insert(100, 3);
-
-        list.display();
-        System.out.println(list.find(2));
-        System.out.println(list.delete(2));
-        list.display();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(13);
+//
+//        list.insertLast(99);
 
 //        list.display();
-//        list.deleteFirst();
-//        System.out.println(list.deleteLast());
-//        list.displaySize();
+//        System.out.println(list.find(2));
+//        System.out.println(list.delete(2));
 //        list.display();
+
+        DoublyLinkedList dList = new DoublyLinkedList();
+
+
+        dList.insertFirst(3);
+        dList.insertFirst(8);
+        dList.insertFirst(12);
+        dList.insertFirst(14);
+        dList.insertFirst(19);
+        dList.insertFirst(21);
+        dList.insertAfter(12, 33);
+
+        dList.display();
     }
 }
