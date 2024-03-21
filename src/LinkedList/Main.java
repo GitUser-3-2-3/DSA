478cccf8b7c03b4b2aac5a633ec7ed7d3d8e24b0
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-03-21 20:26:17 +0530

    Circular Linked List Implementation

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -4,31 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
 
-        LinkedList list = new LinkedList();
+        CircularLinkedList list = new CircularLinkedList();
+        list.insert(23);
+        list.insert(8);
+        list.insert(14);
+        list.insert(45);
+        list.insert(76);
 
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(8);
-//        list.insertFirst(13);
-//
-//        list.insertLast(99);
-
-//        list.display();
-//        System.out.println(list.find(2));
-//        System.out.println(list.delete(2));
-//        list.display();
-
-        DoublyLinkedList dList = new DoublyLinkedList();
-
-
-        dList.insertFirst(3);
-        dList.insertFirst(8);
-        dList.insertFirst(12);
-        dList.insertFirst(14);
-        dList.insertFirst(19);
-        dList.insertFirst(21);
-        dList.insertAfter(12, 33);
-
-        dList.display();
+        list.display();
+        list.delete(14);
+        list.display();
     }
 }
